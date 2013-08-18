efcd14e5e5f9b2925400f05f366f89098388a1f5
jinliangwei <jinliangwei1@gmail.com>
2013-08-19 03:35:49 +0800

    updated C++ definition for NeuronSetInternal, NeuronSetPartition and NeuronScheBlock. updated design for global and local scheduler

--- src/petuum_dnn_neuron_set.hpp
+++ src/petuum_dnn_neuron_set.hpp
@@ -2,17 +2,44 @@
 #define __PETUUM_NEURON_SET_HPP__
 
 #include <stdint.h>
+#include <boost/unordered_map.hpp>
+#include <boost/scoped_array.hpp>
 
 namespace petuum {
+  class NeuronScheBlock;
+  class NeuronSetInternal;
 
-  struct Dimensions {
-    int32_t num_dims_;
-    int32_t *dims_; // size or index along each dimension
+  class Transformation {
+    // the type of transformation:
+    // 1) one patch to one neuron - filtering (overlapped patches) / pooling (non-overlapped patches)
+    // 2) multiple patches to one patch - normalization
+    // 3) one neuron to one neuron - rectification
+
+    // patch size and overlap information
+
+    // The transformation transforms a NeuronSet to another. In other words, the transformation is 1-to-1.
+    // Given the original NeuronSet and the Transformation, the dimensions of the resulting NeuronSet and the dependencies between the two NeuronSets'
+    // NeuronScheBlocks can be determined.
+  };
+
+  enum ENSAggregateType {Add};
+
+  class NeuronSetLineage {
+
+    int num_parents_; // set t 0 if the NeuronSet is directly read from file (like images)
+    boost::scoped_array<int32_t> parent_ids_;
+    Transformation trans_;
+
+    ENSAggregateType aggreg_; // defines how each NeuronSet resulted from transforming a parent NeuronSet are combined together
+                              // only effective when num_parents_ > 1.
+
+    //TODO: add copy constructor - parent_ids_ should be deep copied
   };
 
   template<typename T>
   struct MultiDimensionalArray {
-    Dimensions dim_size_;
+    int32_t num_dims_;
+    int32_t dim_size_; // sizes along each dimensions
     int32_t size_;
     T *data_;
   };
@@ -28,7 +55,11 @@ namespace petuum {
   // NeuronPartition is the unit of data storage and distribution.
   class NeuronPartition {
     int32_t neuron_set_id_;
+    int32_t host_id_;
+    int32_t num_sche_blocks_;
+    boost::scoped_array<int32_t> sche_block_dims_;
 
+    //TODO: constructor
   };
 
   // NeuronScheBlock is a logical partition of a NeuronSet.
@@ -37,29 +68,53 @@ namespace petuum {
   // Each NeuronScheBlock may consists of multiple NeuronPatch or part of a NeuronPatch.
   // Note that NeuronPartition or NeuronScheBlock is not necessarily partitioned along NeuronPatch boundaries, especially as NeuronPatchs may overlap.
   class NeuronScheBlock {
+  private:
     int32_t neuron_set_id_;
     bool is_local_; // if the NeuronScheBlock resides on the same machine
-    Dimensions dim_; // starting index along each dimensions
+    int32_t num_dims_;
+    boost::scoped_array<int32_t> dim_size_;
 
-    float *neurons_; // set to NULL if the NeuronScheBlock is not local and data of it is not received
+    int32_t num_neurons_;
+    int32_t num_updated_neurons_;
 
-    MultiDimensionalArray GetData(Dimensions _dim);
+    int32_t clock_; // version of this block
+
+    // block access to copy constructor
+    NeuronScheBlock(NeuronScheBlock _nsb);
+
+  public:
+
+    // 0 if the updates are accepted
+    // 1 if the updates are accepted and brings the NeuronScheBlock to READY state
+    // negative if error
+    // If a NeuronScheBlock is updated or not completely depends on the number of updated neurons.
+    // so the scheduler should make sure all neurons are updated.
+    int UpdateNeurons(int32_t _num_updated);
 
   };
 
-  class NeuronSet {
-    Dimensions dim_;
+  class NeuronSetInternal {
+  private:
+    int32_t num_dims_;
+    boost::scoped_array<int32_t> dim_size_; // size along each dimensions
 
-    // contains logic for deciding how NeuronSet are split into partitions
-    // contains logic for deciding which NeuronPartition a given NeuronScheBlock belongs to
-  };
+    // partition related
+    boost::scoped_array<int32_t> partition_size_;
+    boost::scoped_array<int32_t> partition_overlap_;
+    boost::scoped_array<NeuronPartition> partitions_;
 
-  class NeuronSetGlobal : public NeuronSet {
+    NeuronSetLineage lineage_;
+    int level_;
 
+    int GetNeurons(MultiDimensionalArray &_data);
+    float GetOneNeuron(int32_t _num_dims, int32_t *_indices);
 
-  };
+    int SetNeurons(const MultiDimensionalArray &data);
+    int SetNeuron(const int32_t *_indices);
 
-  class NeuronSetClient : public NeuronSet {
+    int SetLineage(const NeuronSetLineage &_lineage);
+
+    int Partition();
 
   };
 }
--- src/petuum_dnn_scheduler.hpp
+++ src/petuum_dnn_scheduler.hpp
@@ -8,6 +8,10 @@
 #ifndef __PETUUM_DNN_SCHEDULER_HPP__
 #define __PETUUM_DNN_SCHEDULER_HPP__
 
+#include <boost/unordered_map.hpp>
+
+#include "petuum_dnn_neuron_set.hpp"
+
 namespace petuum {
 
  /* global scheduler work flow:
@@ -16,6 +20,29 @@ namespace petuum {
   * 3) Aggregate the message about NeuronSet partitioning and distributed it to
   *
   */
+
+  class GlobalScheduler {
+
+    std::vector<NeuronSetInternal> neuron_sets_;
+
+    // register a group of NeuronSets
+    int RegisterNeuronSets(int32_t _num_neuron_sets, NeuronSetInternal *_neuron_sets);
+
+    // While setting the transformation, the dimensions and lineage of the NeuronSet is set
+    // Thus the NeuronSet is also partitioned. But the dependency graph is not built.
+    int SetTransformation(int32_t _neuronset_id, const Transformation &_trans);
+
+  };
+
+  // In order to support single machine (one process, multiple threads) and multiple machines (multiple processes),
+  // we let GlobalScheduler talks to LocalScheduler through a proxy which provides a uniform interface for both cases
+  // but does different things.
+  class SchedulerProxy {};
+
+  class LocalScheduler {
+    // need to keep track of all local and some external scheduling blocks
+    // need to keep track of their dependencies (bi-directional) and if the dependencies are ready.
+  };
 
 };
 
